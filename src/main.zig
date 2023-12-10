99b2494a31efdfba6900a1dc1972acc11e3a972b
griffi-gh <prasol258@gmail.com>
2023-12-10 20:20:15 +0100

    accept roms as arg

--- src/main.zig
+++ src/main.zig
@@ -5,19 +5,35 @@ const raylib = @cImport({
 const chip8 = @import("root.zig");
 
 const WIN_SIZE = raylib.Vector2{ .x = 640, .y = 320 };
-const ROM = @embedFile("./_ibm.ch8");
 
 pub fn main() !void {
-    //var allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    _ = try std.io.getStdOut().write("\n");
 
-    // const path = try std.process.argsAlloc(allocator.allocator());
-    // const file = try std.fs.cwd().openFile(path[0], .{});
-    // errdefer file.close();
-    // const data = try file.readToEndAlloc(allocator.allocator(), 0xfff - 0x200);
-    // file.close();
+    var buffer: [0xfff]u8 = undefined;
+
+    const file_path = a: {
+        var fba = std.heap.FixedBufferAllocator.init(&buffer);
+        var args = try std.process.ArgIterator.initWithAllocator(fba.allocator());
+
+        _ = args.skip();
+        const path = args.next() orelse {
+            std.log.err("expected at least one file name as an argument\n", .{});
+            return;
+        };
+
+        break :a path;
+    };
+
+    const file_data = a: {
+        const file = try std.fs.cwd().openFile(file_path, .{});
+        defer file.close();
+
+        const size = try file.readAll(&buffer);
+        break :a buffer[0..size];
+    };
 
     var emu = comptime chip8.Chip8.init();
-    emu.load_rom(ROM);
+    emu.load_rom(file_data);
 
     raylib.SetTraceLogLevel(raylib.LOG_WARNING);
     raylib.SetConfigFlags(raylib.FLAG_VSYNC_HINT);
